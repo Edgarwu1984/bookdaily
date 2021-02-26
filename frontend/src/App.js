d678694d5f92a2c5bf9a9cce004caf582ff8ebc5
Edgarwu1984 <wuzhengjie@gmail.com>
2021-02-26 12:17:51 +1100

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <h3>App component.</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta est sed
+        at quibusdam a qui.
+      </p>
+    </div>
+  );
+}
+
+export default App;
